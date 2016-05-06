c03a7a47eb05e27f53143864cf417786ccd6e7ea
Thanh Vuong <thanh.vuong@yahoo.com>
2016-05-05 23:29:29 -0400

    Initial commit

---
+++ app/src/main/java/edu/quincycollege/csi257/shoppinghelper/ShoppingListActivity.java
@@ -0,0 +1,15 @@
+package edu.quincycollege.csi257.shoppinghelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShoppingListActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+    }
+}
