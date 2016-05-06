be65494f07dbcd6654a686d294084fc99850a90f
Thanh Vuong <thanh.vuong@yahoo.com>
2016-05-06 00:50:47 -0400

    Added Item activity Set coding style to default

--- app/src/main/java/edu/quincycollege/csi257/shoppinghelper/ShoppingListActivity.java
+++ app/src/main/java/edu/quincycollege/csi257/shoppinghelper/ShoppingListActivity.java
@@ -2,14 +2,15 @@ package edu.quincycollege.csi257.shoppinghelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class ShoppingListActivity extends AppCompatActivity
-{
-
+public class ShoppingListActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
