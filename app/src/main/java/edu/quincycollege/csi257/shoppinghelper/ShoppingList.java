3ae51c980cca7af726d2dee028a58f772567f6c3
Mr-Foolish <leechiho22@hotmail.com>
2016-05-08 13:44:21 -0400

    “ShoppingList” class to store Item objects on the list.

---
+++ app/src/main/java/edu/quincycollege/csi257/shoppinghelper/ShoppingList.java
@@ -0,0 +1,49 @@
+package edu.quincycollege.csi257.shoppinghelper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Dark Cube on 5/8/2016.
+ */
+public class ShoppingList {
+    private static ShoppingList sShoppingList;
+
+    public static ShoppingList get() {
+        if (sShoppingList == null)
+            sShoppingList = new ShoppingList();
+        return sShoppingList;
+    }
+    private List<Item> mShoppingList;
+
+
+    private ShoppingList() {
+        mShoppingList = new ArrayList<>();
+    }
+
+    private void addItem(Item newItem){
+        mShoppingList.add(newItem);
+    }
+
+    private void removeItem(UUID id){
+        for (int i = 0; i < mShoppingList.size(); i++)
+            if (mShoppingList.get(i).getId().equals(id))
+                mShoppingList.remove(i);
+    }
+
+    private void clearList(){
+        mShoppingList.clear();
+    }
+
+    public List<Item> getShoppingList() {
+        return mShoppingList;
+    }
+
+    public Item getClothingItem(UUID id) {
+        for (Item item : mShoppingList)
+            if (item.getId().equals(id))
+                return item;
+        return null;
+    }
+}
